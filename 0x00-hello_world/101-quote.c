3161b62ab9bf65373b680ddfe265434c55d75340
seifemad <seifemad100@gmail.com>
2023-02-16 06:24:56 -0800

    df

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
+	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
+    	syscall(SYS_write, 1, msg, sizeof(msg));
 	return (1);
 }
