260b5902681e148085d430c04528907c5b02aa02
seifemad <seifemad100@gmail.com>
2023-02-16 05:37:13 -0800

    ss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-int main()
+int main(void)
 {
-char Mystr[] = "\"Programming is like building a multilingual puzzle";    
-puts(Mystr);    
+char Mystr[]="\"Programming is like building a multilingual puzzle";    
+puts(Mystr); 
 return 0;
 }
