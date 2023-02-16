f81ba14867b918ab91f49918c7e3530b437efe63
Seif Emad El-Din <88642755+seifemadeldin@users.noreply.github.com>
2023-02-16 16:28:01 +0200

    jh

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -6,7 +7,7 @@
  */
 int main(void)
 {
-	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
-    	syscall(SYS_write, 1, msg, sizeof(msg));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
 	return (1);
 }
