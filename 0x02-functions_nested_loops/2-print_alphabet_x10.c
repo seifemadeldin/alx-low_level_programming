ce92d155acbeb4d621928b5ef6d36ed8c972cb7d
Seif Emad El-Din <88642755+seifemadeldin@users.noreply.github.com>
2023-02-21 20:39:05 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * print_alphabet_x10 - a function that prints the alphabet, in lowercase ten times
- *
+ * print_alphabet_x10 - a function that prints the alphabet, in lowercase
+ * ten times
  * Return: Always 0 (Succes)
  */
 void print_alphabet_x10(void)
