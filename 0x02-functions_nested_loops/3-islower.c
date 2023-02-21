852986083d94ba87012dfa5a1f353739a6744c99
Seif Emad El-Din <88642755+seifemadeldin@users.noreply.github.com>
2023-02-21 22:35:51 +0200

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * int _islower(int c) - a function that return 0 or 1
+ *
+ * Return: Always 0 (Succes)
+ */
+void int _islower(int c)
+{
+  if(islower(c) == True)
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
