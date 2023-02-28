d0abbcd3f9cb98035b8fde5c412e32c351defdd6
Seif Emad El-Din <88642755+seifemadeldin@users.noreply.github.com>
2023-02-28 03:54:56 +0200

    sa

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,17 @@
 #include "main.h"
-
 /**
- * puts2 - Prints one char out of two of a string.
- * @str: The string containing characters.
+ * puts2 - Prints out every other char of the string.
+ * @str: input string to print.
  */
 void puts2(char *str)
 {
-	int index = 0, len = 0;
+	int i;
 
-	while (str[index++])
-		len++;
-
-	for (index = 0; index < len; index += 2)
-		_putchar(str[index]);
-
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
 	_putchar('\n');
 }
